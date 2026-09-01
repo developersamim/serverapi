e69b890ff5ca19022829608b872da57a23f304bd
Samim Ahmed <developersamim@gmail.com>
2026-09-01 13:29:19 +1000

    fix infra issue

--- infra/main.tf
+++ infra/main.tf
@@ -24,6 +24,4 @@ resource "azurerm_container_app_environment" "aca_env" {
   location                   = azurerm_resource_group.rg.location
   resource_group_name        = azurerm_resource_group.rg.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
-  log_analytics_workspace_client_id = azurerm_log_analytics_workspace.law.workspace_id
-  log_analytics_workspace_client_secret = azurerm_log_analytics_workspace.law.primary_shared_key
 }
